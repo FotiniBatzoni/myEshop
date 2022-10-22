ed8a46982c21624d6b9f4ccc04345eaedd1a4788
fotini <f.batzoni@gmail.com>
2022-10-22 20:01:21 +0300

    Product Card Component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,18 +1,19 @@
-import { useContext, UseContext } from 'react';
+const { useContext }= require('react');
 
-import { ProductsContext } from '../../context/products.context';
+const { ProductsContext } = require('../../context/products.context');
+const {ProductCard}  = require( '../../components/product-card/product-card.component');
 
 const Shop = () => {
-    const { products } = useContext(ProductsContext)
+    const  {products}  = useContext(ProductsContext);
+
     return (
         <div>
-            {products.map(({id,name}) =>(
-                <div key={id}>
-                    <h1>{name}</h1>
-                </div>
+        {products.map(({product}) =>(
+                <ProductCard key={product.id} product={product}/>
             ))}
         </div>
     )
 }
+
 
 export default Shop;
