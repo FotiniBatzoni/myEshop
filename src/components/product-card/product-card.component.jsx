8026ff352d2acf54c31889ec44c29a6eac1c44e6
fotini <f.batzoni@gmail.com>
2022-10-23 18:39:35 +0300

    Product Card Component

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -5,7 +5,7 @@ import './product-card.styles.scss'
 const ProductCard = ({product}) => {
     const {name,price, imageUrl} = product
     return (
-        <div className='product-card-container'>
+        <div className='.product-card-container'>
             <img src={imageUrl} />
             <div className='footer'>
                 <span className='name'>{name}</span>
