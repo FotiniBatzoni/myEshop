eeba1a5f202ee8539671fa0411aed6604d2be234
fotini <f.batzoni@gmail.com>
2022-10-26 18:14:53 +0300

    Product Cart Styled Component

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,26 +1,37 @@
-import { useContext } from 'react'
+import { useContext } from 'react';
 
-import Button, {BUTTON_TYPE_CLASSES} from '../button/button.component'
-import { CartContext } from '../../context/cart.context'
+import { CartContext } from '../../context/cart.context';
 
-import './product-card.styles.scss'
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 
-const ProductCard = ({product}) => {
-    const {name,price, imageUrl} = product;
-    const { addItemToCart } = useContext(CartContext);
+import {
+  ProductCartContainer,
+  Footer,
+  Name,
+  Price,
+} from './product-card.styles';
 
-    const addProductToCart = () => addItemToCart(product)
+const ProductCard = ({ product }) => {
+  const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
 
-    return (
-        <div className='.product-card-container'>
-            <img src={imageUrl} />
-            <div className='footer'>
-                <span className='name'>{name}</span>
-                <span className='price'>{price}</span>
-            </div>
-            <Button buttonType={BUTTON_TYPE_CLASSES.inverted} onClick={addProductToCart}>Add to card</Button>
-        </div>
-    )
-}
+  const addProductToCart = () => addItemToCart(product);
 
-export default ProductCard
+  return (
+    <ProductCartContainer>
+      <img src={imageUrl} alt={`${name}`} />
+      <Footer>
+        <Name>{name}</Name>
+        <Price>{price}</Price>
+      </Footer>
+      <Button
+        buttonType={BUTTON_TYPE_CLASSES.inverted}
+        onClick={addProductToCart}
+      >
+        Add to card
+      </Button>
+    </ProductCartContainer>
+  );
+};
+
+export default ProductCard;
