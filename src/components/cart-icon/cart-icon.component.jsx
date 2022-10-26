4407c16943e697ae5bb6af74e2457a2d28a023c5
fotini <f.batzoni@gmail.com>
2022-10-26 14:44:38 +0300

    Cart Icon Styled Component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,12 +1,12 @@
 import { useContext } from 'react'
 
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
-
 import { CartContext } from '../../context/cart.context';
 
 import {
-    CartIconContainer 
-} from'./cart-icon.styles.jsx'
+    CartIconContainer,
+    ShoppingIcon,
+    ItemCount
+} from'./cart-icon.styles'
 
 const CartIcon = () => {
     const { isCartOpen, setIsCartOpen, cartCount} = useContext(CartContext);
@@ -16,7 +16,7 @@ const CartIcon = () => {
     return(
         <CartIconContainer onClick={toggleIsCartOpen}>
             <ShoppingIcon className='shopping-icon' />
-            <span className='item-count'>{cartCount}</span>
+            <ItemCount>{cartCount}</ItemCount>
         </CartIconContainer>
     )
 }
--- src/components/cart-icon/cart-icon.styles.jsx
+++ src/components/cart-icon/cart-icon.styles.jsx
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
+import { ReactComponent as ShoppingSvg } from '../../assets/shopping-bag.svg';
+
+export const ShoppingIcon = styled(ShoppingSvg)`
+      width: 24px;
+      height: 24px;
+`
+
 export const CartIconContainer = styled.div`
   width: 45px;
   height: 45px;
@@ -9,6 +16,13 @@ export const CartIconContainer = styled.div`
   justify-content: center;
   cursor: pointer;
 `;
+
+export const ItemCount = styled.span`
+      position: absolute;
+      font-size: 10px;
+      font-weight: bold;
+      bottom: 12px;
+`
 
 
 // .cart-icon-container {
