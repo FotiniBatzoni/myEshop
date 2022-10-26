f90b266cca0df6810b23839b5dba59150ec79229
fotini <f.batzoni@gmail.com>
2022-10-26 13:42:54 +0300

    Cart-Dropdown with styled-components

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,7 +6,9 @@ import { CartContext } from '../../context/cart.context';
 import Button from '../button/button.component'
 import CartItem from '../cart-item/cart-item.component';
 
-import './cart-dropdown.styles.scss';
+import {
+    CartDropdownContainer
+} from './cart-dropdown.styles.jsx';
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
@@ -17,14 +19,14 @@ const CartDropdown = () => {
     }
 
  return(
-    <div className='cart-dropdown-container'>
+    <CartDropdownContainer>
         <div className='cart-items'>
             {cartItems.map(item =>( 
                 <CartItem key={item.id} cartItem={item}/>
             ))}
         </div>
         <Button onClick = {goToCheckoutHandler}>GO TO CHECKOUT</Button>
-    </div>
+    </CartDropdownContainer>
  )   
 };
 
