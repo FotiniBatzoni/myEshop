530fedb2f81252b653697d561bb22d9f883cf512
fotini <f.batzoni@gmail.com>
2022-10-26 14:10:31 +0300

    Button with styled components

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import  FormInput from '../form-input/form-input.component';
-import Button from "../button/button.component";
+import Button, {BUTTON_TYPE_CLASSES} from "../button/button.component";
 import './sign-in-form.styles.scss';
 
 import { 
@@ -65,7 +65,10 @@ const SignInForm = () =>{
                     <Button type="submit">
                         Sign In
                     </Button>
-                    <Button type='button' buttonType='google' onClick = {signInWithGoogle}>
+                    <Button 
+                        buttonType={BUTTON_TYPE_CLASSES.google} 
+                        type='button'
+                        onClick = {signInWithGoogle}>
                         Google Sign In
                     </Button>
                 </div>
